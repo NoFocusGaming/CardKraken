16a02c6d50cbda68ae536ee90446430466e10c39
Julianne Maghirang <70534153+Maghirang-Julianne@users.noreply.github.com>
2023-05-06 03:44:42 -0700

    Read Discord Update 3am for all details

--- Assets/Scripts/Managers/CameraMgr.cs
+++ Assets/Scripts/Managers/CameraMgr.cs
@@ -37,7 +37,9 @@ public class CameraMgr : MonoBehaviour
 
             //very basic camera movement forwards
             if (!obstacle && Input.GetKeyUp(KeyCode.W))
-                cameraObj.transform.Translate(Vector3.forward * cameraMoveSpeed);
+                // i added "multiply by 5" so it goes farther instead of player having to press W button multiple times to get to each card
+                // feel free to change or back to original one - jules
+                cameraObj.transform.Translate(Vector3.forward * cameraMoveSpeed * 5);
         }
     }
 
