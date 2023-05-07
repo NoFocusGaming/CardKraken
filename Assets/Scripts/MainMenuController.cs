d187f30d972b180d0d2dba3f19f568c35b42f942
Julianne Maghirang <70534153+Maghirang-Julianne@users.noreply.github.com>
2023-05-06 17:17:20 -0700

    Read Discord update 5pm for all details

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -20,35 +20,6 @@ public class MainMenuController : MonoBehaviour
         audioSource.PlayOneShot(buttonClickSound);
     }
 
-    public void loadTutorial()
-    {
-        SceneManager.LoadScene("TutorialCardWorld");
-        audioSource.PlayOneShot(buttonClickSound);
-    }
-
-    public void loadVillage()
-    {
-        SceneManager.LoadScene("VillageCardWorld");
-        audioSource.PlayOneShot(buttonClickSound);
-    }
-
-    public void loadLevel1()
-    {
-        SceneManager.LoadScene("Level1CardWorld");
-    }
-
-    public void loadLevel2()
-    {
-        SceneManager.LoadScene("Level2CardWorld");
-        audioSource.PlayOneShot(buttonClickSound);
-    }
-
-    public void loadBoss()
-    {
-        SceneManager.LoadScene("BossCardWorld");
-        audioSource.PlayOneShot(buttonClickSound);
-    }
-
     public void quit()
     {
         Application.Quit();
